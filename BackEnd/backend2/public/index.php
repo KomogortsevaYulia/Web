092d153331833b4d6185dd3c33947f659c31f2b9
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2021-10-07 10:25:15 +0800

    начала разбиратьтся с twig

---
+++ BackEnd/backend2/public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+// подключаем пакеты которые установили через composer
+require_once '../vendor/autoload.php';
+
+// создаем загрузчик шаблонов, и указываем папку с шаблонами
+// \Twig\Loader\FilesystemLoader -- это типа как в C# писать Twig.Loader.FilesystemLoader, 
+// только слеш вместо точек
+$loader = new \Twig\Loader\FilesystemLoader('../views');
+
+// создаем собственно экземпляр Twig с помощью которого будет рендерить
+$twig = new \Twig\Environment($loader);
+
+$url = $_SERVER["REQUEST_URI"];
+
+if ($url == "/") {
+    // это убираем require "../views/main.html";
+    
+    echo $twig->render("main.html");
+} elseif (preg_match("#/mermaid#", $url)) {
+    // и это тоже require "../views/mermaid.html";
+    
+    echo $twig->render("mermaid.html");
+} elseif (preg_match("#/uranus#", $url)) {
+    // и вот это require "../views/uranus.html";
+    
+    echo $twig->render("uranus.html");
+}
+?>
