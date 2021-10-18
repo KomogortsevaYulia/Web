4e79ae99243fef6dd176629f18fc754a8a697a13
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2021-10-18 11:10:28 +0800

    +

--- BackEnd/backend2 copy/controllers/SpathiphyllumImageController.php
+++ BackEnd/backend2 copy/controllers/SpathiphyllumImageController.php
@@ -7,7 +7,7 @@ class SpathiphyllumImageController extends SpathiphyllumController {
     public function getContext(): array
     {
         $context = parent::getContext(); 
-            $context['image'] = "/images/img-Spatif.jpg";
+        $context['image'] = "/images/img-Spatif.jpg";
         $context['text'] = file_get_contents("C:/Yulia/Study/3_course/Web/BackEnd/backend1/views/Spathiphyllum-info.php");
         $context['type'] = "картинка";
         return $context;
