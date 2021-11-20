9d8411f343a692736135d75c7b62d976aebd8587
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2021-11-20 10:26:30 +0800

    +

--- BackEnd/backendDB/controllers/SearchController.php
+++ BackEnd/backendDB/controllers/SearchController.php
@@ -9,17 +9,27 @@ class SearchController extends BasePlantsTwigController {
         $context = parent::getContext();
         $type=isset($_GET['type']) ? $_GET['type']:'';
         $title=isset($_GET['title']) ? $_GET['title']:'';
-
-        $sql =<<<EOL
-        SELECT * 
-        from flowers
-        WHERE (:title=''OR title like CONCAT('%',:title,'%')) and (type=:type)
+        $info=isset($_GET['info']) ? $_GET['info']:'';
+        if($type=="Все"){
+            $sql =<<<EOL
+            SELECT * 
+            from flowers
+            WHERE (:title=''OR title like CONCAT('%',:title,'%')) and (:info=''OR info like CONCAT('%',:info,'%'))
 EOL;
+        }else{
+            $sql =<<<EOL
+            SELECT * 
+            from flowers
+            WHERE (:title=''OR title like CONCAT('%',:title,'%')) and (type=:type) and (:info=''OR info like CONCAT('%',:info,'%'))
+EOL;
+        }
+        
         $query=$this->pdo->prepare($sql);
         $query->bindValue("title",$title);
         $query->bindValue("type",$type);
+        $query->bindValue("info",$info);
         $query->execute();
-
+        $context['get']=$_GET;
         $context['flowers']=$query->fetchAll();
         
         return $context;
