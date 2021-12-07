dc0a81a46cc6645fbdb7b9ef895a923663cf9351
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2021-12-07 15:13:16 +0800

    +

---
+++ BackEnd/backendDB/controllers/LogInController.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../framework/TwigBaseController.php';
+class LogInController extends TwigBaseController {
+    public $template = "logIn.twig"; // указываем шаблон
+
+    public function post(array $context) {
+         // берем значения которые введет пользователь
+         $user = isset($_POST['user']) ? $_POST['user'] : '';
+         $password = isset($_POST['password']) ? $_POST['password'] : '';
+         
+        
+         // создам запрос, под параметр создаем переменную my_id в запросе
+         $query = $this->pdo->prepare("SELECT * FROM users WHERE username= :user and password =:password");
+         // подвязываем значение в my_id 
+         $query->bindValue("user", $user);
+         $query->bindValue("password", $password);
+         $query->execute(); // выполняем запрос
+         // тянем данные
+         $data = $query->fetch();
+
+         // сверяем с корректными
+         if ($data['id']=='') {
+                $_SESSION['is_logged']=false;
+             exit; // прерываем выполнение скрипта
+         }else{
+            $_SESSION['is_logged']=true;
+         }
+    }
+
+}
--- BackEnd/backendDB/public/index.php
+++ BackEnd/backendDB/public/index.php
@@ -5,6 +5,7 @@ require_once '../vendor/autoload.php';
 require_once '../framework/autoload.php';
 require_once '../middlewares/LoginRequiredMiddeware.php';
 require_once '../middlewares/HistoryMiddeware.php';
+require_once '../controllers/LogInController.php';
 // require_once '../framework/BaseRestController.php';
 require_once "../controllers/MainController.php"; // добавим в самом верху ссылку на наш контроллер
 require_once "../controllers/Controller404.php"; // добавил
@@ -33,7 +34,7 @@ $router->add("/types/create", TypesCreateController::class)->middleware(new Logi
 $router->add("/types", TypesController::class);
 $router->add("/flower/(?P<id>\d+)/delete", ObjectDeleteController::class)->middleware(new LoginRequiredMiddeware());
 $router->add("/flower/(?P<id>\d+)/edit", ObjectUpdateController::class)->middleware(new LoginRequiredMiddeware());
-
+$router->add("/logIn", LogInController::class);
 
 // $router->add("/flower-api", BaseRestController::class);
 // $router->add("/flower-api/(?P<id>\d+)", BaseRestController::class);
